792f7f81c4090fdc0e1207ff1a499ad6adcf84bd
hp-pc <navyashree811@gmail.com>
2022-08-21 21:23:48 +0530

    Added : Welcome to Stack and Queue Problem

---
+++ src/com/bridgelabz/stackqueue/WelcomeToStackQueue.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.stackqueue;
+
+public class WelcomeToStackQueue {   
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stacks & Queues Problems");
+	}
+
+}
